585798fc0889619702297b5f7ee2c0c95473ce2f
avanishonline <94173182+avanishonline@users.noreply.github.com>
2022-05-25 17:45:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log{"First console project"}
+var first_name = "RANVIR";
+var last_name = "KAPOOR";
+var full_name = first_name.concat{last_name};
+console.log{full_name};
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log{add};
+var sub = a-b;
+console.log{sub};
